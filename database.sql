365b089f21a6aee31035fb9ef4c5d548a1d82d63
mebius <mebius1080p@gmail.com>
2020-01-03 11:50:35 +0900

    データベース初期化サンプル sql を追加

---
+++ database.sql
@@ -0,0 +1,16 @@
+-- db バージョンアップ時など用初期化 sql サンプル
+
+CREATE DATABASE hoge;
+
+-- ユーザー作成
+CREATE USER fuga IDENTIFIED BY `xxxxxxxxxx`;
+
+-- 確認
+SELECT Host, User, Password FROM mysql.user;
+
+
+-- 権限付与
+GRANT ALL ON hoge.* TO fuga;
+
+-- 確認
+SHOW GRANTS FOR fuga;
