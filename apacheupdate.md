3b7fe83cf8477cdd62fa06a5f4a9239333120c89
mebius <mebius1080p@gmail.com>
2021-10-06 22:02:55 +0900

    apache 更新の際のヘルパースクリプトなどを追加

---
+++ apacheupdate.md
@@ -0,0 +1,19 @@
+# apache update helper
+- do following command after execute apacheupdate.sh
+
+```sh
+# shutdown container
+cd ~/docker \
+&& sudo docker-compose down
+
+# remove old container image
+sudo docker images
+sudo docker rmi xxxxxxxxxxx
+
+# edit apache/dockerfile
+# apache バージョン更新
+# nghttp2 のバージョンも更新がある場合は更新する
+
+# build
+sudo docker-compose up -d
+```
---
+++ apacheupdate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# helper script for apache update
+
+APACHE_VERSION="2.4.50"
+
+# prepare spec file
+cd ~/ \
+&& mkdir -p download \
+&& cd ~/download \
+&& wget https://www-eu.apache.org/dist/httpd/httpd-${APACHE_VERSION}.tar.bz2 \
+&& tar xvf httpd-${APACHE_VERSION}.tar.bz2
+
+# rename current httpd.spec to compare and fix
+cd ~/docker/apache/spec \
+&& mv httpd.spec httpd.spec.old \
+&& cp ~/download/httpd-${APACHE_VERSION}/httpd.spec ~/docker/apache/spec
+
+# ここで httpd.spec を調整する
+# バージョン確認、必要な行を追加する
+# 作業が終わったら
+# rm ~/docker/apache/spec/httpd.spec.old
+# 第一段階終了
