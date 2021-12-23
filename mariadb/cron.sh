e1e63406db5e6fc2149d828530630dc2ad8db8c9
mebius <mebius1080p@gmail.com>
2021-12-23 20:35:48 +0900

    cron スクリプトを修正

--- mariadb/cron.sh
+++ mariadb/cron.sh
@@ -5,20 +5,20 @@ DC_PATH_FILE=~/compose_dir.txt
 
 # compose_dir.txt 存在チェック
 if [ ! -e $DC_PATH_FILE ]; then
-	echo `date +%Y-%m-%d_%H:%M:%S` compose_dir.txt not found >> $LOG_FILE
+	echo "$(date +%Y-%m-%d_%H:%M:%S)" compose_dir.txt not found >> $LOG_FILE
 	exit 1;
 fi
 
 # ファイル内容のディレクトリチェック
-dc_path=`cat $DC_PATH_FILE`
-if [ ! -d $dc_path ]; then
-	echo `date +%Y-%m-%d_%H:%M:%S` contents of compose_dir.txt is not dir path >> $LOG_FILE
+dc_path=$(cat $DC_PATH_FILE)
+if [ ! -d "$dc_path" ]; then
+	echo "$(date +%Y-%m-%d_%H:%M:%S)" contents of compose_dir.txt is not dir path >> $LOG_FILE
 	exit 1;
 fi
 
 # dump タスク
-echo `date +%Y-%m-%d_%H:%M:%S` start dump >> $LOG_FILE
+echo "$(date +%Y-%m-%d_%H:%M:%S)" start dump >> $LOG_FILE
 
-docker exec docker_mariadb_1 /var/scripts/dump.sh
+docker exec docker-mariadb-1 /var/scripts/dump.sh
 
-echo `date +%Y-%m-%d_%H:%M:%S` end dump >> $LOG_FILE
+echo "$(date +%Y-%m-%d_%H:%M:%S)" end dump >> $LOG_FILE
